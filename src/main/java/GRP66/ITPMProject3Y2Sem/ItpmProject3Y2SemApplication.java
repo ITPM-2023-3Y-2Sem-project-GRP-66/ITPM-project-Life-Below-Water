ef8568e52b1b81350bebd9315764b94d0fefe6d1
chathumax <73824269+IT20194208@users.noreply.github.com>
2023-03-25 11:52:02 +0530

    create articl related files inthe main structure(controller,dto,entity..) and enable swaggger configeration

--- src/main/java/GRP66/ITPMProject3Y2Sem/ItpmProject3Y2SemApplication.java
+++ src/main/java/GRP66/ITPMProject3Y2Sem/ItpmProject3Y2SemApplication.java
@@ -2,10 +2,12 @@ package GRP66.ITPMProject3Y2Sem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@Configuration
 
 public class ItpmProject3Y2SemApplication {
 
